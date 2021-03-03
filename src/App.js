34589bc628a2df40bbf63f0d8033a0b87273afe7
Mehdi <mehdi.regragui@ummahcharity.org>
2021-03-03 10:46:57 +0100

    add navbar

--- src/App.js
+++ src/App.js
@@ -15,6 +15,26 @@ function App() {
   if (count !== 0) {
     return (
       <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Game</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Score</a>
+              </li>
+
+            </ul>
+          </div>
+        </nav>
         <div className="row">
           <h1>Calcul mental!</h1>
         </div>
